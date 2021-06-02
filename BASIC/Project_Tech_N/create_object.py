50bfe8e7a922310efe27863c2dd0886b63320f32
Frycu128 <79200852+Frycu128@users.noreply.github.com>
2021-06-02 16:50:07 +0200

    Delete create_object.py

--- BASIC/Project_Tech_N/create_object.py
+++
@@ -1,9 +0,0 @@
-class plates:
-    def __init__(self, number, name, drawing_number, thickness, x, y, grade):
-        self.number = number
-        self.name = name
-        self.drawing_number = drawing_number
-        self.grade = grade
-        self.weight = thickness * (x/1000) * (y/1000) * 7,85
-
-
