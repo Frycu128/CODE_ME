b6baf83a88f6f0ecd8b58c633dd631e1165923a5
Frycu128 <79200852+Frycu128@users.noreply.github.com>
2021-06-02 17:18:25 +0200

    Delete test.py

--- BASIC/Project_Tech_N/test.py
+++
@@ -1,6 +0,0 @@
-class Plates:
-    def __init__(self, number, drawing_number, thickness, x, y, grade):
-        self.number = number
-        self.drawing_number = drawing_number
-        self.grade = grade
-        self.weight = 1
